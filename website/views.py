20dea280decdfdf9df809c8278659be8bff26805
Jesswill <jessica_williams0@protonmail.com>
2024-10-23 19:34:58 -0400

    index routing work

--- website/views.py
+++ website/views.py
@@ -1,7 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return '<h1>Starter code for assignment 3<h1>'
+    return render_template('index.html')
+
+@main_bp.route('/create-event')
+def create_event():
+    return render_template('create_event.html')
+
+@main_bp.route('/event-details')
+def event_details():
+    return render_template('event_details.html')
+
+@main_bp.route('/booking_history')
+def booking_history():
+    return render_template('booking_history.html')
